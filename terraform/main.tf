c9a8686f2b3735d69ad64e2faa728bfcb7caa324
Ezan25 <ezanmoh1@gmail.com>
2026-09-17 05:55:08 +0100

    fix: remove duplicate ecs module block, add depends_on for listener ordering

--- terraform/main.tf
+++ terraform/main.tf
@@ -24,17 +24,6 @@ module "alb" {
   certificate_arn   = module.acm.certificate_arn
 }
 
-module "ecs" {
-  source = "./modules/ecs"
-
-  vpc_id                = module.vpc.vpc_id
-  public_subnet_ids     = module.vpc.public_subnet_ids
-  alb_security_group_id = module.alb.security_group_id
-  target_group_arn      = module.alb.target_group_arn
-  ecr_repository_url    = module.ecr.repository_url
-  aws_region            = "eu-west-2"
-}
-
 resource "aws_route53_record" "app" {
   zone_id = module.acm.zone_id
   name    = var.domain_name
@@ -45,4 +34,16 @@ resource "aws_route53_record" "app" {
     zone_id                = module.alb.alb_zone_id
     evaluate_target_health = true
   }
+}
+module "ecs" {
+  source = "./modules/ecs"
+
+  vpc_id                = module.vpc.vpc_id
+  public_subnet_ids     = module.vpc.public_subnet_ids
+  alb_security_group_id = module.alb.security_group_id
+  target_group_arn      = module.alb.target_group_arn
+  ecr_repository_url    = module.ecr.repository_url
+  aws_region             = "eu-west-2"
+
+  depends_on = [module.alb]
 }
